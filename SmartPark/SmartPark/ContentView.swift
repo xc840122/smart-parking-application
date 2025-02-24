4d8ef5db58614f2e8482b24a5bd1afe6b9818eb6
Yongfu Lin <felixlin0907@gmail.com>
2025-02-24 13:08:16 +1300

    create iOS project

---
+++ SmartPark/SmartPark/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartPark
+//
+//  Created by 阿福 on 24/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartPark/SmartPark/SmartParkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartParkApp.swift
+//  SmartPark
+//
+//  Created by 阿福 on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SmartParkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
