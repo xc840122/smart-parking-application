baf345b25e519ab7c66a01579b0aa4f853de34e0
Chi Xu <xuchi007@hotmail.com>
2025-03-15 00:30:43 +1300

    update booking page, bugfix, change userid to clerkUserId

--- web/src/app/(user)/booking/page.tsx
+++ web/src/app/(user)/booking/page.tsx
@@ -6,15 +6,15 @@ import SignInPage from "@/app/sign-in/[[...sign-in]]/page";
 
 const BookingPage = async () => {
   // Get clerk user ID
-  const { userId, role } = await userHelper();
+  const { clerkUserId, role } = await userHelper();
 
-  if (!userId) {
+  if (!clerkUserId) {
     return <SignInPage />;
   }
 
   return (
     <Suspense fallback={<Loading />}>
-      <BookingWrapper clerkUserId={userId} role={role} />
+      <BookingWrapper clerkUserId={clerkUserId} role={role} />
     </Suspense>
   );
 };
