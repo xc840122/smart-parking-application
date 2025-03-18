46be546bb77062d53f6d685cf6b70b24873f9096
Chi Xu <xuchi007@hotmail.com>
2025-03-18 23:19:20 +1300

    remove login request for parking

--- web/src/app/(user)/parking/page.tsx
+++ web/src/app/(user)/parking/page.tsx
@@ -3,7 +3,6 @@ import ParkingSpaceWrapper from "./parking-wrapper";
 import Loading from "@/components/Loading";
 import { userHelper } from "@/helper/user.helper";
 import { getCitiesService } from "@/services/address.service";
-import { toast } from "sonner";
 
 
 const ParkingSpacePage = async () => {
@@ -11,12 +10,6 @@ const ParkingSpacePage = async () => {
   const { role } = await userHelper()
   // Get cities
   const cities = await getCitiesService();
-  // If role is not found, return loading
-  if (!role) return <Loading />;
-  if (!cities.result) {
-    toast.message(cities.message);
-    return <Loading />;
-  }
 
   return (
     <Suspense fallback={<Loading />}>
